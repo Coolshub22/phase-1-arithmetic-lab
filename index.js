b506ae608cd2f3966dab6a455007b0798804262b
brian23ka <brianngenye@gmail.com>
2025-03-12 05:58:42 +0300

    perform arithmetic in index.js

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
 
-// Write your code here
+console.log(multiply);
+
+let random = Math.floor(Math.random() * 100) + 1;
+console.log(random);
+
+let num3 = 14;
+let num4 = 5;
+let mod = num3 % num4;
+
+console.log(mod);
+
+let max = Math.max(10, 5, 20, 15);
+
+console.log(max);
